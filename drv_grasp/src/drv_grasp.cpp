20101e3592338f2a2119726bb459ebb62153a4cf
Zhipeng Dong <zhipengdongneu@gmail.com>
2016-11-29 14:30:37 +0800

    Add cmd_vel function

--- drv_grasp/src/drv_grasp.cpp
+++ drv_grasp/src/drv_grasp.cpp
@@ -18,6 +18,8 @@
 #include <std_msgs/UInt16MultiArray.h>
 #include <visualization_msgs/Marker.h>
 
+#include <geometry_msgs/Pose2D.h>
+
 //Custom message
 #include <drv_msgs/recognized_target.h>
 
@@ -40,6 +42,7 @@ typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloud;
 ros::Publisher graspPubMarker_;
 ros::Publisher graspPubStatus_;
 ros::Publisher graspPubCloud_;
+ros::Publisher graspPubPose_;
 
 enum ModeType{m_wander, m_search, m_track};
 int modeType_ = m_wander;
@@ -47,6 +50,8 @@ int modeTypeTemp_ = m_wander;
 
 string param_running_mode = "/status/running_mode";
 bool hasGraspPlan_ = false;
+
+bool posePublished_ = false;
 
 // target pointcloud index of image
 #ifdef USE_CENTER
@@ -77,103 +82,103 @@ void getCloudByInliers(PointCloud::Ptr cloud_in, PointCloud::Ptr &cloud_out,
 
 void msgToCloud(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr cloud_in, pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud_out)
 {
-		cloud_out->height = cloud_in->height;
-		cloud_out->width  = cloud_in->width;
-		cloud_out->is_dense = false;
-		cloud_out->resize(cloud_out->height * cloud_out->width);
+    cloud_out->height = cloud_in->height;
+    cloud_out->width  = cloud_in->width;
+    cloud_out->is_dense = false;
+    cloud_out->resize(cloud_out->height * cloud_out->width);
 
-		for (size_t i = 0; i < cloud_out->size(); ++i)
-				{
-						cloud_out->points[i].x = cloud_in->points[i].x;
-						cloud_out->points[i].y = cloud_in->points[i].y;
-						cloud_out->points[i].z = cloud_in->points[i].z;
-						cloud_out->points[i].r = cloud_in->points[i].r;
-						cloud_out->points[i].g = cloud_in->points[i].g;
-						cloud_out->points[i].b = cloud_in->points[i].b;
-				}
+    for (size_t i = 0; i < cloud_out->size(); ++i)
+    {
+        cloud_out->points[i].x = cloud_in->points[i].x;
+        cloud_out->points[i].y = cloud_in->points[i].y;
+        cloud_out->points[i].z = cloud_in->points[i].z;
+        cloud_out->points[i].r = cloud_in->points[i].r;
+        cloud_out->points[i].g = cloud_in->points[i].g;
+        cloud_out->points[i].b = cloud_in->points[i].b;
+    }
 }
 
 void doTransform(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_in, pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud_out, const geometry_msgs::TransformStamped& t_in)
 {
     Eigen::Transform<float,3,Eigen::Affine> t = Eigen::Translation3f(t_in.transform.translation.x, t_in.transform.translation.y,t_in.transform.translation.z)
-                                         * Eigen::Quaternion<float>(t_in.transform.rotation.w, t_in.transform.rotation.x, t_in.transform.rotation.y, t_in.transform.rotation.z);
+            * Eigen::Quaternion<float>(t_in.transform.rotation.w, t_in.transform.rotation.x, t_in.transform.rotation.y, t_in.transform.rotation.z);
 
     Eigen::Vector3f point;
 
-		cloud_out->height = cloud_in->height;
-		cloud_out->width  = cloud_in->width;
-		cloud_out->is_dense = false;
-		cloud_out->resize(cloud_out->height * cloud_out->width);
+    cloud_out->height = cloud_in->height;
+    cloud_out->width  = cloud_in->width;
+    cloud_out->is_dense = false;
+    cloud_out->resize(cloud_out->height * cloud_out->width);
 
-		for (size_t i = 0; i < cloud_in->size(); ++i)
-				{
-						point = t * Eigen::Vector3f(cloud_in->points[i].x, cloud_in->points[i].y, cloud_in->points[i].z);
-						cloud_out->points[i].x = point.x();
-						cloud_out->points[i].y = point.y();
-						cloud_out->points[i].z = point.z();
-						cloud_out->points[i].r = cloud_in->points[i].r;
-						cloud_out->points[i].g = cloud_in->points[i].g;
-						cloud_out->points[i].b = cloud_in->points[i].b;
-				}
+    for (size_t i = 0; i < cloud_in->size(); ++i)
+    {
+        point = t * Eigen::Vector3f(cloud_in->points[i].x, cloud_in->points[i].y, cloud_in->points[i].z);
+        cloud_out->points[i].x = point.x();
+        cloud_out->points[i].y = point.y();
+        cloud_out->points[i].z = point.z();
+        cloud_out->points[i].r = cloud_in->points[i].r;
+        cloud_out->points[i].g = cloud_in->points[i].g;
+        cloud_out->points[i].b = cloud_in->points[i].b;
+    }
 }
 
 void doTransform(pcl::PointXYZRGB p_in, pcl::PointXYZRGB &p_out, const geometry_msgs::TransformStamped& t_in)
 {
     Eigen::Transform<float,3,Eigen::Affine> t = Eigen::Translation3f(t_in.transform.translation.x, t_in.transform.translation.y,t_in.transform.translation.z)
-                                         * Eigen::Quaternion<float>(t_in.transform.rotation.w, t_in.transform.rotation.x, t_in.transform.rotation.y, t_in.transform.rotation.z);
+            * Eigen::Quaternion<float>(t_in.transform.rotation.w, t_in.transform.rotation.x, t_in.transform.rotation.y, t_in.transform.rotation.z);
 
     Eigen::Vector3f point;
 
-		point = t * Eigen::Vector3f(p_in.x, p_in.y, p_in.z);
-		p_out.x = point.x();
-		p_out.y = point.y();
-		p_out.z = point.z();
-		p_out.r = p_in.r;
-		p_out.g = p_in.g;
-		p_out.b = p_in.b;
+    point = t * Eigen::Vector3f(p_in.x, p_in.y, p_in.z);
+    p_out.x = point.x();
+    p_out.y = point.y();
+    p_out.z = point.z();
+    p_out.r = p_in.r;
+    p_out.g = p_in.g;
+    p_out.b = p_in.b;
 }
 
 void trackResultCallback(const drv_msgs::recognized_targetConstPtr & msg)
 {
-		if (modeType_ != m_track)
-				{
-						return;
-				}
+    if (modeType_ != m_track)
+    {
+        return;
+    }
 
 #ifdef USE_CENTER
-		// directly use center as tgt location
-		idx_ = msg->tgt_bbox_center.data[0] + msg->tgt_bbox_center.data[1] * 640;
+    // directly use center as tgt location
+    idx_ = msg->tgt_bbox_center.data[0] + msg->tgt_bbox_center.data[1] * 640;
 #else
-		inliers_->indices.clear();
-		// use mask as tgt area
-		for (size_t i = 0; i < msg->tgt_pixels.data.size(); i++)
-				{
-						inliers_->indices.push_back(msg->tgt_pixels.data[i]);
-				}
+    inliers_->indices.clear();
+    // use mask as tgt area
+    for (size_t i = 0; i < msg->tgt_pixels.data.size(); i++)
+    {
+        inliers_->indices.push_back(msg->tgt_pixels.data[i]);
+    }
 #endif
 }
 
 void cloudCallback(const PointCloud::ConstPtr& msg)
 {
-		if (modeType_ != m_track)
-				{
-						return;
-				}
+    if (modeType_ != m_track)
+    {
+        return;
+    }
 
-		if (msg->empty())
-				{
-						ROS_ERROR("Can't get source cloud message.\n");
-						return;
-				}
+    if (msg->empty())
+    {
+        ROS_ERROR("Can't get source cloud message.\n");
+        return;
+    }
 
 #ifdef USE_CENTER
 
 #else
-		if (inliers_->indices.empty())
-				{
-						ROS_WARN_THROTTLE(5, "Object grasp plan has not been found.\n");
-						return;
-				}
+    if (inliers_->indices.empty())
+    {
+        ROS_WARN_THROTTLE(5, "Object grasp plan has not been found.\n");
+        return;
+    }
 #endif
 
     MakePlan MP;
@@ -184,9 +189,9 @@ void cloudCallback(const PointCloud::ConstPtr& msg)
     if (isnan(p_in.x) || isnan(p_in.y) || isnan(p_in.z))
         hasGraspPlan_ = false;
     else {
-            doTransform(p_in, avrPt, transformStamped_);
-            hasGraspPlan_ = MP.smartOffset(avrPt, 0.02);
-        }
+        doTransform(p_in, avrPt, transformStamped_);
+        hasGraspPlan_ = MP.smartOffset(avrPt, 0.02);
+    }
 #else
     PointCloud::Ptr cloud_in (new PointCloud());
     msgToCloud(msg, cloud_in);
@@ -197,110 +202,122 @@ void cloudCallback(const PointCloud::ConstPtr& msg)
     PointCloud::Ptr cloud_fo (new PointCloud ());
     MP.removeNans(cloud_fl, cloud_fo);
 
-		// transform the point cloud
-		PointCloud::Ptr cloud_ts (new PointCloud ());
-		doTransform(cloud_fo, cloud_ts, transformStamped_);
+    // transform the point cloud
+    PointCloud::Ptr cloud_ts (new PointCloud ());
+    doTransform(cloud_fo, cloud_ts, transformStamped_);
 
-		cloud_ts->header.stamp = msg->header.stamp;
-		cloud_ts->header.frame_id = targetFrame_;
-		graspPubCloud_.publish(*cloud_ts);
+    cloud_ts->header.stamp = msg->header.stamp;
+    cloud_ts->header.frame_id = targetFrame_;
+    graspPubCloud_.publish(*cloud_ts);
 
-		hasGraspPlan_ = MP.process(cloud_ts, pa_, pb_, pc_, pd_, avrPt);
+    hasGraspPlan_ = MP.process(cloud_ts, pa_, pb_, pc_, pd_, avrPt);
 #endif
 
-		if (hasGraspPlan_)
-				{
-						visualization_msgs::Marker marker;
-						// Set the frame ID and timestamp.  See the TF tutorials for information on these.
-						marker.header.frame_id = targetFrame_;
-						marker.header.stamp = pcl_conversions::fromPCL(msg->header.stamp);
+    geometry_msgs::Pose2D pose;
 
-						// Set the namespace and id for this marker.  This serves to create a unique ID
-						// Any marker sent with the same namespace and id will overwrite the old one
-						marker.ns = "grasp";
-						marker.id = 0;
+    if (hasGraspPlan_)
+    {
+        visualization_msgs::Marker marker;
+        // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+        marker.header.frame_id = targetFrame_;
+        marker.header.stamp = pcl_conversions::fromPCL(msg->header.stamp);
 
-						marker.type = shape;
-						marker.action = visualization_msgs::Marker::ADD;
+        // Set the namespace and id for this marker.  This serves to create a unique ID
+        // Any marker sent with the same namespace and id will overwrite the old one
+        marker.ns = "grasp";
+        marker.id = 0;
 
-						// Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-						marker.points.resize(2);
+        marker.type = shape;
+        marker.action = visualization_msgs::Marker::ADD;
 
-						marker.points[0].x = avrPt.x;
-						marker.points[0].y = avrPt.y;
-						marker.points[0].z = avrPt.z;
+        // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+        marker.points.resize(2);
 
-						marker.points[1].x = avrPt.x;
-						marker.points[1].y = avrPt.y;
-						marker.points[1].z = avrPt.z + 0.15; // arrow height = 0.15m
+        marker.points[0].x = avrPt.x;
+        marker.points[0].y = avrPt.y;
+        marker.points[0].z = avrPt.z;
 
-						// The point at index 0 is assumed to be the start point, and the point at index 1 is assumed to be the end.
+        pose.x = avrPt.x;
+        pose.y = avrPt.y;
+        graspPubPose_.publish(pose);
 
-						// scale.x is the shaft diameter, and scale.y is the head diameter. If scale.z is not zero, it specifies the head length.
-						// Set the scale of the marker -- 1x1x1 here means 1m on a side
-						marker.scale.x = 0.01;
-						marker.scale.y = 0.015;
-						marker.scale.z = 0.04;
+        marker.points[1].x = avrPt.x;
+        marker.points[1].y = avrPt.y;
+        marker.points[1].z = avrPt.z + 0.15; // arrow height = 0.15m
 
-						// Set the color -- be sure to set alpha to something non-zero!
-						marker.color.r = 0.0f;
-						marker.color.g = 1.0f;
-						marker.color.b = 0.0f;
-						marker.color.a = 1.0;
+        // The point at index 0 is assumed to be the start point, and the point at index 1 is assumed to be the end.
 
-						graspPubMarker_.publish(marker);
-				}
-		else
-				{
-						ROS_WARN_THROTTLE(5, "Failed to generate grasp plan.\n");
-				}
+        // scale.x is the shaft diameter, and scale.y is the head diameter. If scale.z is not zero, it specifies the head length.
+        // Set the scale of the marker -- 1x1x1 here means 1m on a side
+        marker.scale.x = 0.01;
+        marker.scale.y = 0.015;
+        marker.scale.z = 0.04;
+
+        // Set the color -- be sure to set alpha to something non-zero!
+        marker.color.r = 0.0f;
+        marker.color.g = 1.0f;
+        marker.color.b = 0.0f;
+        marker.color.a = 1.0;
+
+        graspPubMarker_.publish(marker);
+    }
+    else
+    {
+        pose.x = 0;
+        pose.y = 0;
+        graspPubPose_.publish(pose);
+        ROS_WARN_THROTTLE(5, "Failed to generate grasp plan.\n");
+    }
 }
 
 int main(int argc, char **argv)
 {
-		ros::init(argc, argv, "drv_grasp");
+    ros::init(argc, argv, "drv_grasp");
 
-		ros::NodeHandle nh;
+    ros::NodeHandle nh;
 
-		graspPubMarker_ = nh.advertise<visualization_msgs::Marker>("grasp/marker", 1);
-		graspPubStatus_ = nh.advertise<std_msgs::Bool>("status/grasp/feedback", 1);
-		graspPubCloud_ = nh.advertise<sensor_msgs::PointCloud2>("grasp/pointcloud", 1);
+    graspPubMarker_ = nh.advertise<visualization_msgs::Marker>("grasp/marker", 1);
+    graspPubStatus_ = nh.advertise<std_msgs::Bool>("status/grasp/feedback", 1);
+    graspPubCloud_ = nh.advertise<sensor_msgs::PointCloud2>("grasp/pointcloud", 1);
+    graspPubPose_ = nh.advertise<geometry_msgs::Pose2D>("grasp/target_pose", 1);
 
-		tf2_ros::Buffer tfBuffer_;
-		tf2_ros::TransformListener tfListener_(tfBuffer_);
+    tf2_ros::Buffer tfBuffer_;
+    tf2_ros::TransformListener tfListener_(tfBuffer_);
 
-		ros::Subscriber sub_track = nh.subscribe<drv_msgs::recognized_target>("track/recognized_target", 1, trackResultCallback);
-		ros::Subscriber sub_clouds = nh.subscribe<PointCloud>("/point_cloud", 1, cloudCallback);
+    ros::Subscriber sub_track = nh.subscribe<drv_msgs::recognized_target>("track/recognized_target", 1, trackResultCallback);
+    ros::Subscriber sub_clouds = nh.subscribe<PointCloud>("/point_cloud", 1, cloudCallback);
 
-		ROS_WARN("Grasping plan function initialized!\n");
+    ROS_WARN("Grasping plan function initialized!\n");
 
-		while (ros::ok())
-				{
-						if (ros::param::has(param_running_mode))
-								{
-										ros::param::get(param_running_mode, modeTypeTemp_);
-										modeType_ = modeTypeTemp_;
-								}
+    while (ros::ok())
+    {
+        if (ros::param::has(param_running_mode))
+        {
+            ros::param::get(param_running_mode, modeTypeTemp_);
+            if (modeType_ == m_wander && modeTypeTemp_ != m_wander)
+                posePublished_ = false;
+            modeType_ = modeTypeTemp_;
+        }
 
-						std_msgs::Bool flag;
-						flag.data = true;
+        std_msgs::Bool flag;
+        flag.data = true;
 
-						try
-						{
-								transformStamped_ = tfBuffer_.lookupTransform(targetFrame_, sourceFrame_, ros::Time(0));
-						}
-						catch (tf2::TransformException &ex)
-						{
-								ROS_WARN("%s",ex.what());
-								ros::Duration(1.0).sleep();
-								continue;
-						}
+        try
+        {
+            transformStamped_ = tfBuffer_.lookupTransform(targetFrame_, sourceFrame_, ros::Time(0));
+        }
+        catch (tf2::TransformException &ex)
+        {
+            ROS_WARN("%s",ex.what());
+            ros::Duration(1.0).sleep();
+            continue;
+        }
 
-						ros::spinOnce();
+        ros::spinOnce();
 
-						flag.data = hasGraspPlan_;
-						graspPubStatus_.publish(flag);
-				}
+        flag.data = hasGraspPlan_;
+        graspPubStatus_.publish(flag);
+    }
 
-		return 0;
+    return 0;
 }
