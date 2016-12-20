c39fc2a7e6f126012b9792bf51c0ec896504db5b
DrawZeroPoint <damoninhit@qq.com>
2016-12-20 20:46:29 +0800

    Bug fix

--- drv_tf/src/drv_tf.cpp
+++ drv_tf/src/drv_tf.cpp
@@ -15,6 +15,9 @@
 #include <message_filters/sync_policies/approximate_time.h>
 #include <message_filters/subscriber.h>
 
+#include <image_transport/image_transport.h>
+#include <image_transport/subscriber_filter.h>
+
 #include <dynamic_reconfigure/server.h>
 #include <drv_tf/tfConfig.h>
 
@@ -82,6 +85,78 @@ void imuCallback(const std_msgs::Float32ConstPtr & msg)
     pitch_ = imuPitchTemp * 0.01745; // 3.14159 / 180.0
 }
 
+void imageCallback(const sensor_msgs::ImageConstPtr &image_msg)
+{
+		static tf2_ros::TransformBroadcaster br_optic_to_link;
+		geometry_msgs::TransformStamped transformStamped1;
+		transformStamped1.header.stamp = image_msg->header.stamp;
+		transformStamped1.header.frame_id = cameraLinkFrameID_;
+		transformStamped1.child_frame_id = cameraOpticalFrameID_;
+		transformStamped1.transform.translation.x = dx_optic_to_link;
+		transformStamped1.transform.translation.y = dy_optic_to_link;
+		transformStamped1.transform.translation.z = dz_optic_to_link;
+		tf2::Quaternion q;
+		q.setRPY(3*M_PI_2 , 0, 3*M_PI_2);
+		transformStamped1.transform.rotation.x = q.x();
+		transformStamped1.transform.rotation.y = q.y();
+		transformStamped1.transform.rotation.z = q.z();
+		transformStamped1.transform.rotation.w = q.w();
+		br_optic_to_link.sendTransform(transformStamped1);
+
+    static tf2_ros::TransformBroadcaster br_link_to_pitch;
+    geometry_msgs::TransformStamped transformStamped2;
+    transformStamped2.header.stamp = transformStamped1.header.stamp;
+    transformStamped2.header.frame_id = cameraPitchFrameID_;
+    transformStamped2.child_frame_id = cameraLinkFrameID_;
+    transformStamped2.transform.translation.x = dx_link_to_pitch;
+    transformStamped2.transform.translation.y = dy_link_to_pitch;
+    transformStamped2.transform.translation.z = dz_link_to_pitch;
+    tf2::Quaternion q2;
+    q2.setRPY(0 , pitch_, 0);
+    q2.setY(- q2.y());
+    transformStamped2.transform.rotation.x = q2.x();
+    transformStamped2.transform.rotation.y = q2.y();
+    transformStamped2.transform.rotation.z = q2.z();
+    transformStamped2.transform.rotation.w = q2.w();
+    br_link_to_pitch.sendTransform(transformStamped2);
+
+    static tf2_ros::TransformBroadcaster br_pitch_to_yaw;
+    geometry_msgs::TransformStamped transformStamped3;
+    transformStamped3.header.stamp = transformStamped1.header.stamp;
+    transformStamped3.header.frame_id = cameraYawFrameID_;
+    transformStamped3.child_frame_id = cameraPitchFrameID_;
+    transformStamped3.transform.translation.x = dx_pitch_to_yaw;
+    transformStamped3.transform.translation.y = dy_pitch_to_yaw;
+    transformStamped3.transform.translation.z = dz_pitch_to_yaw;
+    tf2::Quaternion q3;
+    q3.setRPY(0 , 0, yaw_);
+    transformStamped3.transform.rotation.x = q3.x();
+    transformStamped3.transform.rotation.y = q3.y();
+    transformStamped3.transform.rotation.z = q3.z();
+    transformStamped3.transform.rotation.w = q3.w();
+    br_pitch_to_yaw.sendTransform(transformStamped3);
+
+    static tf2_ros::TransformBroadcaster br_yaw_to_base;
+    geometry_msgs::TransformStamped transformStamped4;
+    transformStamped4.header.stamp = transformStamped1.header.stamp;
+#ifdef DEBUG_TRANS
+    transformStamped4.header.frame_id = "/map";
+#else
+    transformStamped4.header.frame_id = baseLinkFrameID_;
+#endif
+    transformStamped4.child_frame_id = cameraYawFrameID_;
+    transformStamped4.transform.translation.x = dx_yaw_to_base_;
+    transformStamped4.transform.translation.y = dy_yaw_to_base_;
+    transformStamped4.transform.translation.z = dz_yaw_to_base_;
+    tf2::Quaternion q4;
+    q4.setRPY(0, 0, 0);
+    transformStamped4.transform.rotation.x = q4.x();
+    transformStamped4.transform.rotation.y = q4.y();
+    transformStamped4.transform.rotation.z = q4.z();
+    transformStamped4.transform.rotation.w = q4.w();
+    br_yaw_to_base.sendTransform(transformStamped4);
+}
+
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "drv_tf");
@@ -101,91 +176,20 @@ int main(int argc, char** argv){
 		server.setCallback(f);
 
 		ros::Subscriber sub_yaw = nh.subscribe<std_msgs::UInt16MultiArray> (visionNameSpaceID_ + "/servo", 1, servoCallback);
-		ros::Subscriber sub_acc = nh.subscribe(visionNameSpaceID_ + "/camera_pitch", 3, imuCallback);
+		ros::Subscriber sub_acc = nh.subscribe(visionNameSpaceID_ + "/camera_pitch", 1, imuCallback);
 
-		 ROS_INFO("TF initialized.");
+		ros::NodeHandle rgb_nh(nh, "rgb");
+		ros::NodeHandle rgb_pnh(pnh, "rgb");
+		image_transport::ImageTransport it_rgb_sub(rgb_nh);
+		image_transport::TransportHints hints_rgb("compressed", ros::TransportHints(), rgb_pnh);
+		image_transport::Subscriber sub_rgb = it_rgb_sub.subscribe("/rgb/image", 1, imageCallback, hints_rgb);
+
+
+    ROS_INFO("TF initialized.");
 
     while (ros::ok())
         {
             ros::spinOnce();
-
-            static tf2_ros::TransformBroadcaster br_optic_to_link;
-            geometry_msgs::TransformStamped transformStamped1;
-
-            transformStamped1.header.stamp = ros::Time::now();
-            transformStamped1.header.frame_id = cameraLinkFrameID_;
-            transformStamped1.child_frame_id = cameraOpticalFrameID_;
-
-            transformStamped1.transform.translation.x = dx_optic_to_link;
-            transformStamped1.transform.translation.y = dy_optic_to_link;
-            transformStamped1.transform.translation.z = dz_optic_to_link;
-            tf2::Quaternion q;
-            q.setRPY(3*M_PI_2 , 0, 3*M_PI_2);
-            transformStamped1.transform.rotation.x = q.x();
-            transformStamped1.transform.rotation.y = q.y();
-            transformStamped1.transform.rotation.z = q.z();
-            transformStamped1.transform.rotation.w = q.w();
-
-            br_optic_to_link.sendTransform(transformStamped1);
-
-            static tf2_ros::TransformBroadcaster br_link_to_pitch;
-            geometry_msgs::TransformStamped transformStamped2;
-
-            transformStamped2.header.stamp = transformStamped1.header.stamp;
-            transformStamped2.header.frame_id = cameraPitchFrameID_;
-            transformStamped2.child_frame_id = cameraLinkFrameID_;
-            transformStamped2.transform.translation.x = dx_link_to_pitch;
-            transformStamped2.transform.translation.y = dy_link_to_pitch;
-            transformStamped2.transform.translation.z = dz_link_to_pitch;
-            tf2::Quaternion q2;
-            q2.setRPY(0 , pitch_, 0);
-            q2.setY(- q2.y());
-            transformStamped2.transform.rotation.x = q2.x();
-            transformStamped2.transform.rotation.y = q2.y();
-            transformStamped2.transform.rotation.z = q2.z();
-            transformStamped2.transform.rotation.w = q2.w();
-
-            br_link_to_pitch.sendTransform(transformStamped2);
-
-            static tf2_ros::TransformBroadcaster br_pitch_to_yaw;
-            geometry_msgs::TransformStamped transformStamped3;
-
-            transformStamped3.header.stamp = transformStamped1.header.stamp;
-            transformStamped3.header.frame_id = cameraYawFrameID_;
-            transformStamped3.child_frame_id = cameraPitchFrameID_;
-            transformStamped3.transform.translation.x = dx_pitch_to_yaw;
-            transformStamped3.transform.translation.y = dy_pitch_to_yaw;
-            transformStamped3.transform.translation.z = dz_pitch_to_yaw;
-            tf2::Quaternion q3;
-            q3.setRPY(0 , 0, yaw_);
-            transformStamped3.transform.rotation.x = q3.x();
-            transformStamped3.transform.rotation.y = q3.y();
-            transformStamped3.transform.rotation.z = q3.z();
-            transformStamped3.transform.rotation.w = q3.w();
-
-            br_pitch_to_yaw.sendTransform(transformStamped3);
-
-            static tf2_ros::TransformBroadcaster br_yaw_to_base;
-            geometry_msgs::TransformStamped transformStamped4;
-            transformStamped4.header.stamp = transformStamped1.header.stamp;
-        #ifdef DEBUG_TRANS
-            transformStamped4.header.frame_id = "/map";
-        #else
-            transformStamped4.header.frame_id = baseLinkFrameID_;
-        #endif
-            transformStamped4.child_frame_id = cameraYawFrameID_;
-
-            transformStamped4.transform.translation.x = dx_yaw_to_base_;
-            transformStamped4.transform.translation.y = dy_yaw_to_base_;
-            transformStamped4.transform.translation.z = dz_yaw_to_base_;
-            tf2::Quaternion q4;
-            q4.setRPY(0 , 0, 0);
-            transformStamped4.transform.rotation.x = q4.x();
-            transformStamped4.transform.rotation.y = q4.y();
-            transformStamped4.transform.rotation.z = q4.z();
-            transformStamped4.transform.rotation.w = q4.w();
-            br_yaw_to_base.sendTransform(transformStamped4);
-
         }
     return 0;
 };
